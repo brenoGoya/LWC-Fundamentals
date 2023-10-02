7b4e74ce9debcb5ddde2d3352caecb7d269d7725
Breno Goya <breno.goya@gmail.com>
2023-10-02 15:17:23 -0300

    foreach loop

--- force-app/main/default/lwc/helloQuerySelector/helloQuerySelector.js
+++ force-app/main/default/lwc/helloQuerySelector/helloQuerySelector.js
@@ -1,8 +1,18 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloQuerySelector extends LightningElement {
+    
+    userNames = ["John", "Smith", "Nik", "Mike"]
+    
     fetchDetailHandler() {
         const elem = this.template.querySelector('h1')
+
+        const userElements = this.template.querySelectorAll('.name')
+
         console.log(elem.innerText)
+
+        Array.from(userElements).forEach(item => {
+            console.log(item.innerText)
+        });
     }
 }
